aeac2e99a62da9b9dd70cf2585ef9b44002bd2ad
anhtnq98 <kethanlinh.otaku@gmail.com>
2023-03-10 08:58:36 +0700

    fix

--- js/color-vietsub.js
+++ js/color-vietsub.js
@@ -1,16 +1,3 @@
-// 7 sắc cầu vồng
-// listViet[0] = "Màu đỏ <br><br> - Màu đỏ là một gam màu rực rỡ, nó dễ dàng thu hút mọi ánh nhìn.  <br><br> - Đây là gam màu tượng trưng cho tình yêu cháy bỏng, cho đam mê bùng cháy và nhiệt huyết dâng trào. <br><br> - Nhưng ở một mặt khác, màu đỏ lại gợi lên cảm giác nguy hiểm, phưu lưu. Màu sắc này là tượng trưng cho máu và lửa.  <br><br> - Về góc độ tâm linh, màu đỏ cũng là một trong những gam màu huyền thuật. Nó ẩn chứa sức mạnh và mang theo nguồn năng lượng to lớn.";
-// listViet[1] = "Màu vàng <br><br> - Màu vàng là màu tốt nhất để tăng sự nhiệt tình trong cuộc sống của bạn và có thể đóng góp với sự tự tin và lạc quan hơn. <br><br> - Nó thích những thách thức, đặc biệt là các loại tinh thần.";
-// listViet[2] = "Màu xanh nước biển <br><br> - Màu xanh dương hay còn được biết đến là xanh nước biển, xanh da trời.  <br><br> - Đây là màu sắc được rất nhiều người ưa thích bởi tính dịu mát, nhẹ nhàng mà nó mang lại.";
-// listViet[3] = "Màu cam <br><br> Năng lượng, cân bằng, ấm áp, nhiệt tình, sức sống, mở rộng, rực rỡ, mùa thu và Halloween.";
-// listViet[4] = "Màu xanh lá cây <br><br> - Xanh lá cây hay còn gọi là xanh lục. <br><br> - Là màu sắc đại diện cho cây cối núi rừng.  <br><br> - Là biểu tượng của sự an toàn.";
-// listViet[5] = "Màu tím <br><br> Quý phái, tâm linh, lễ, bí ẩn, biến đổi, trí tuệ và giác ngộ.";
-// listViet[6] = "Màu nâu <br><br> Đất, lò sưởi, nhà, ngoài trời, độ tin cậy, thoải mái, bền bỉ, ổn định, đơn giản, thoải mái, cây cối, thiên nhiên và mùa thu."
-// listViet[7] = "Màu đen <br><br> Sức mạnh, tinh tế, trang trọng, thanh lịch, giàu có, bí ẩn, sợ hãi, xấu xa, ẩn danh, bất hạnh, sâu sắc, phong cách, buồn, hối hận, giận dữ, ngầm, kỹ thuật, đau buồn và cái chết."
-// listViet[8] = "Màu trắng <br><br> Sự tôn kính, trinh tiết, hư vô, sạch sẽ, hòa bình, khiêm tốn, chính xác, hồn nhiên, tuổi trẻ, sinh, mùa đông, tuyết, lòng tốt, hôn nhân, lạnh, lâm sàng và vô trùng.";
-// listViet[9] = "Màu xám <br><br> An toàn, đáng tin cậy, thông minh, u sầu, khiêm tốn, nhân phẩm, trưởng thành, lành mạnh, chức năng, tuổi già và đau buồn.";
-// listViet[100] = "Màu <br><br>"
-
 // list tiếng Anh
 listEng[0] = "red";
 listEng[1] = "yellow";
